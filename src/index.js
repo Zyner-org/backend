652f6d19f39f470642960f13964663d56309fced
Vermium Sifell <vermium@zyner.org>
2021-03-20 14:53:57 +0100

    ✨ respond to get /

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log("Backend is running on port", port);
