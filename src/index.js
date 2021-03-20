9419a4b33321708acf2fc937931ae6c87fe99f1c
Vermium Sifell <vermium@zyner.org>
2021-03-20 14:52:07 +0100

    🔊 log listen from app

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {});
+app.listen(port, () => {
+  console.log("Backend is running on port", port);
+});
